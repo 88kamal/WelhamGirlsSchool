87c3c5e5819b1d3ff8e8078cab4343fa236afaed
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2022-10-28 14:07:39 +0530

    commit3

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import '../stylesheet/Carousels.css'
 
 function Carousels() {
- 
+
   return (
     <div >
 
@@ -21,46 +21,52 @@ function Carousels() {
             <img src="/images/slider/Mobile LP-3.jpg" className="image2 w-100 " alt="..." />
           </div>
         </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true" ></span>
+        <div>
+          <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon none1" aria-hidden="true" ></span>
           <span className="visually-hidden">Previous</span>
         </button>
+
         <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true" ></span>
+          <span className="carousel-control-next-icon none1" aria-hidden="true" ></span>
           <span className="visually-hidden" >Next</span>
         </button>
+        </div>
+        
         <div className="form-container container my-2">
-        <form id="contact" className="kwes-form" action="https://formsubmit.co/Tarun.enquiry@gmail.com" method="POST">
-          <h3 style={{ color: '#337ab7', fontWeight: 'bold', paddingTop: '10px', textTransform: 'uppercase', fontSize: '20px' }}> Get More Information About Admissions</h3>
-          {/* <h4 style="font-weight: bold; text-transform: uppercase;">Contact us</h4> */}
-          <fieldset>
-            <input name="name" placeholder="Your name" type="text" tabIndex={1} required />
-            <input type="hidden" name="_captcha" value="false" />
+          <form id="contact" className="kwes-form" action="https://formsubmit.co/Tarun.enquiry@gmail.com" method="POST">
+            <h3 style={{ color: '#337ab7', fontWeight: 'bold', paddingTop: '10px', textTransform: 'uppercase', fontSize: '20px' }}> Get More Information About Admissions</h3>
+            {/* <h4 style="font-weight: bold; text-transform: uppercase;">Contact us</h4> */}
+            <fieldset>
+              <input name="name" placeholder="Your name" type="text" tabIndex={1} required />
+              <input type="hidden" name="_captcha" value="false" />
 
-            <input type="hidden" name="_next" value="http://localhost:3000/thanks" />
+              <input type="hidden" name="_next" value="http://localhost:3000/thanks" />
 
 
-          </fieldset>
-          <fieldset>
-            <input name="email" placeholder="Your Email Address" type="email" tabIndex={2} required />
-          </fieldset>
-          <fieldset>
-            <input name="PhoneNO" placeholder="Your Phone Number" type="tel" tabIndex={3} required />
-          </fieldset>
-          {/* <fieldset>
+            </fieldset>
+            <fieldset>
+              <input name="email" placeholder="Your Email Address" type="email" tabIndex={2} required />
+            </fieldset>
+            <fieldset>
+              <input name="PhoneNO" placeholder="Your Phone Number" type="tel" tabIndex={3} required />
+            </fieldset>
+            {/* <fieldset>
             <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required>
           </fieldset> */}
-          <fieldset>
-            <textarea name="Message" placeholder="Type your message here...." tabIndex={5} required defaultValue={""} />
-          </fieldset>
-          <fieldset>
-            <button type="submit" id="contact-submit" data-submit="...Sending" onClick={alert}>Submit</button>
-          </fieldset>
-        </form>
+            <fieldset>
+              <textarea name="Message" placeholder="Type your message here...." tabIndex={5} required defaultValue={""} />
+            </fieldset>
+            <fieldset>
+              <button type="submit" id="contact-submit" data-submit="...Sending" onClick={alert}>Submit</button>
+            </fieldset>
+          </form>
+        </div>
+       
       </div>
-      </div>
+      
 
-    
+
     </div>
   )
 }
