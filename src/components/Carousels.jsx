19e8a8782e337504ba55f641770723db08c885e6
KAMAL NAYAN UPADHYAY <knupadhyay784@gmail.com>
2022-12-10 13:09:21 +0530

    all done

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -13,40 +13,40 @@ function Carousels() {
       <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel" >
         <div className="carousel-inner">
           <div className="carousel-item active">
-            <img src="/images/slider/LP- 1.jpg" className=" image1  w-100" alt="..."  />
+            <img src="/images/slider/LP- 1.jpg" className=" image1  w-100" alt="..." />
             <img src="/images/slider/Mobile LP-1.jpg" className=" image2  w-100 " alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="/images/slider/LP-2.jpg" className=" image1  w-100" alt="..."  />
+            <img src="/images/slider/LP-2.jpg" className=" image1  w-100" alt="..." />
             <img src="/images/slider/Mobile LP-2.jpg" className="image2 w-100  " alt="..." />
           </div>
           <div className="carousel-item">
-            <img src="/images/slider/LP-3.jpg" className=" image1 w-100" alt="..."  />
+            <img src="/images/slider/LP-3.jpg" className=" image1 w-100" alt="..." />
             <img src="/images/slider/Mobile LP-3.jpg" className="image2 w-100 " alt="..." />
           </div>
         </div>
         <div>
           <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon none1" aria-hidden="true" ></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
+            <span className="carousel-control-prev-icon none1" aria-hidden="true" ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
 
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-          <span className="carousel-control-next-icon none1" aria-hidden="true" ></span>
-          <span className="visually-hidden" >Next</span>
-        </button>
+          <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span className="carousel-control-next-icon none1" aria-hidden="true" ></span>
+            <span className="visually-hidden" >Next</span>
+          </button>
         </div>
-        
-        {/* <div className="form-container container my-2">
-          <form id="contact" className="kwes-form" action="https://formsubmit.co/Tarun.enquiry@gmail.com" method="POST">
-            <h3 style={{ color: '#337ab7', fontWeight: 'bold', paddingTop: '10px', textTransform: 'uppercase', fontSize: '20px' }}> Get More Information About Admissions</h3>
+
+      
+
+        <div className="form-container my-2">
+          <form id="contact" className="kwes-form" action="https://kwesforms.com/api/foreign/forms/EcAQXYendrmbM6keh0Cw" method="post" noValidate="novalidate">
+            <div className="kw-alert kw-alert-success" id="kw-success" aria-live="assertive" style={{ display: 'none' }} />
+            <h3 style={{ color: '#337ab7', fontWeight: 'bold', paddingTop: '10px', textTransform: 'uppercase', fontSize: '20px' }}>
+              Apply For Admissions</h3>
+            {/* <h4 style="font-weight: bold; text-transform: uppercase;">Contact us</h4> */}
             <fieldset>
-              <input name="name" placeholder="Your name" type="text" tabIndex={1} required />
-              <input type="hidden" name="_captcha" value="false" />
-
-              <input type="hidden" name="_next" value="http://localhost:3000/thanks" />
-
-
+              <input name="name" placeholder="Your name" type="text" tabIndex={1} required className="kw-border-error" />
             </fieldset>
             <fieldset>
               <input name="email" placeholder="Your Email Address" type="email" tabIndex={2} required />
@@ -54,45 +54,20 @@ function Carousels() {
             <fieldset>
               <input name="PhoneNO" placeholder="Your Phone Number" type="tel" tabIndex={3} required />
             </fieldset>
-            
-            <fieldset>
-              <textarea name="Message" placeholder="Type your message here...." tabIndex={5} required defaultValue={""} />
-            </fieldset>
-            <fieldset>
-              <button type="submit" id="contact-submit" data-submit="...Sending" onClick={alert}>Submit</button>
-            </fieldset>
-          </form>
-        </div> */}
-
-<div className="form-container my-2">
-        <form id="contact" className="kwes-form" action="https://kwesforms.com/api/foreign/forms/KFxLSjT83EHVTcM9Whom" method="post" noValidate="novalidate">
-          <div className="kw-alert kw-alert-success" id="kw-success" aria-live="assertive" style={{display: 'none'}} />
-          <h3 style={{color: '#337ab7', fontWeight: 'bold', paddingTop: '10px', textTransform: 'uppercase', fontSize: '20px'}}>
-            Apply For Admissions</h3>
-          {/* <h4 style="font-weight: bold; text-transform: uppercase;">Contact us</h4> */}
-          <fieldset>
-            <input name="name" placeholder="Your name" type="text" tabIndex={1} required className="kw-border-error" />
-          </fieldset>
-          <fieldset>
-            <input name="email" placeholder="Your Email Address" type="email" tabIndex={2} required />
-          </fieldset>
-          <fieldset>
-            <input name="PhoneNO" placeholder="Your Phone Number" type="tel" tabIndex={3} required />
-          </fieldset>
-          {/* <fieldset>
+            {/* <fieldset>
             <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required>
           </fieldset> */}
-          {/* <fieldset>
+            {/* <fieldset>
                 <textarea name="Message" placeholder="Type your message here...." tabindex="5" required></textarea>
             </fieldset> */}
-          <fieldset>
-            <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-          </fieldset>
-        </form>
+            <fieldset>
+              <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+            </fieldset>
+          </form>
+        </div>
+
       </div>
-       
-      </div>
-      
+
 
 
     </div>
--- src/pages/SimilarSchool.jsx
+++ src/pages/SimilarSchool.jsx
@@ -2,127 +2,109 @@ import React from 'react'
 import '../stylesheet/Similarschool.css'
 
 function SimilarSchool() {
+
+    const des1 = "Kareena Kapoor Khan, a former student of Welham Girls School, has had an incredible journey throughout her time spent at Welham.Kareena was raised in Mumbai by her mother, Babita, who encouraged her daughter to pursue an education diligently and sent her to study at Welham Girls School at the age of 14."
+    const des2 = " Welham Girls School alumna Meira Singh is well-known for her tireless work ethic and several accomplishments. As a result of attending the best girls' boarding school in India, Meira Kapoor developed into a self-assured young woman. She acquired most of her leadership skills at her alma institution."
+    const des4 = " Welham has many precious, noble jewels in its tiara. One of them is Tavleen Singh, who entered the gates of Welhams with a smile on her face. Born and brought up amidst the hills, her parents always taught her to stay grounded. She was a woman with a strong mind who always knew what she wanted to pursue as a career. "
+    const des3 = "Brinda Karat is a noteworthy alumna of the Welham Girls School who has made many headlines with her active contribution to Indian politics. Brinda was just born after the Independence in October I947 is a lady of ambition and holds a tiara of a diverse portfolio."
     return (
         <div className=' container addmission' >
-            {/* <div className="main">
-        <div className="card">
-          <div>
-            <img className="flexbox" src="./images/home/Mayo.jpeg" />
-            <h3 style={{color: 'black', fontWeight: 500, marginTop: '10px'}}>Mayo College Girls’ School</h3>
-            <p style={{marginBottom: '13px'}}>Mayo College Girls’ School, take pride in its "exceptional level of pastoral care and academics  There is a 33% chance of getting admission at MCGS.<br /> Grab this opportunity now!</p>
-            <a className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="flexbox" src="./images/home/Ecole.jpeg" />
-            <h3 style={{color: 'black', fontWeight: 500, marginTop: '8px'}}>Ecole Globale International Girls’ School</h3>
-            <p style={{marginBottom: '1px !important'}}>Ecole Globale has an excellent academic record. It offers foreign exchange programs, earning them better opportunities. You still have 41% chance to get admission.</p>
-            <a className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a>
-          </div>
-        </div>
-        <div className="card">
-          <div>
-            <img className="flexbox" src="./images/home/scindia.jpeg" />
-            <h3 style={{color: 'black', fontWeight: 500, marginTop: '10px'}}>Scindia Kanya Vidyalaya</h3>
-            <p style={{marginBottom: '12px'}}>Scindia Kanya Vidyalaya is a premier girls' school, that helps children to grow both academically and personally.  There is a 40% chance of your child, to study here.</p>
-            <a className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a>
-          </div>
-        </div>
-        <div className="card">
-          <form style={{borderRadius: '10px', height: '550px !important', width: '280px !important'}} id="contact" className="kwes-form" action="https://kwesforms.com/api/foreign/forms/KFxLSjT83EHVTcM9Whom" method="post" noValidate="novalidate">
-            <div className="kw-alert kw-alert-success" id="kw-success" aria-live="assertive" style={{display: 'none'}} />
-            <h3 style={{color: '#337ab7', fontWeight: 'bold', paddingTop: '10px', textTransform: 'uppercase', fontSize: '20px'}}>
-              Apply For Admissions</h3>
-            <input style={{marginTop: '5px'}} name="name" placeholder="Your name" type="text" tabIndex={1} required autoFocus />
-            <input style={{marginTop: '5px'}} name="email" placeholder="Your Email Address" type="email" tabIndex={2} required />
-            <input style={{marginTop: '5px'}} name="PhoneNO" placeholder="Your Phone Number" type="tel" tabIndex={3} required />
-            <button className="btn-one" style={{marginBottom: '28px', marginTop: '20px'}} name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-          </form>
-        </div>
-      </div> */}
+
             <h2 className="copyright title-head" style={{ color: '#848484', marginTop: '20px' }}>Similar School   <span style={{ marginLeft: '8px', color: '#337ab7', marginBottom: '30px' }}>Options</span>
             </h2>
 
             <div className="row  similar">
-                <div className="col-md-6 col-lg-3 my-3">
-                    <div className="card border-bottom-2"  >
-                        <img src="./images/home/Mayo.jpeg" className="card-img-top" alt="..." style={{
-                            borderTopRightRadius: '12px',
-                            borderTopLeftRadius: '12px',
-                        }} />
-                        <div className="card-body card_h" style={{ borderRadius: '12px' }}>
-                            <h3 className="card-title  text-center" style={{ fontWeight: '500' }}>Mayo College Girls’ School</h3>
-                            <p className="card-text" style={{
-                                textAlign: 'justify',
-                                textJustify: 'interWord',
-                            }}>Mayo College Girls’ School, take pride in its "exceptional level of pastoral care and academics There is a 33% chance of getting admission at MCGS.
-                                Grab this opportunity now!</p>
-                            <div className="center">
-                                {/* <a href='/' style={{
-                                    position: 'relative',
-                                    top: '29px'
-                                }} className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a> */}
-                                <button className="btn-one btn_1" name="submit" type="submit" id="contact-submit" data-submit="...Sending">APPLY NOW</button>
-                            </div>                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6 col-lg-3 my-3">
-                    <div className="card" >
-                        <img src="./images/home/Ecole.jpeg" className="card-img-top" alt="..." style={{
-                            borderTopRightRadius: '12px',
-                            borderTopLeftRadius: '12px',
-                        }} />
-                        <div className="card-body card_h" style={{ borderRadius: '12px' }} >
-                            <h3 className="card-title  text-center" style={{ fontWeight: '500' }}>Ecole Globale International Girls’ School</h3>
-                            <p className="card-text " style={{
-                                textAlign: 'justify',
-                                textJustify: 'interWord',
-                                letterSpacing: 'px'
-                            }}  >Ecole Globale has an excellent academic record. It offers foreign exchange programs, earning them better opportunities.You still have 41% chance to get admission.</p>
-                            <div className="center">
-                                {/* <a href='/' className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a> */}
-                                <button className="btn-one" name="submit" type="submit" id="contact-submit" data-submit="...Sending">APPLY NOW</button>
-                            </div>                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6 col-lg-3 my-3">
+            <div className="col-md-6 col-lg-3 my-3">
                     <div className="card " style={{ border: 'none !important' }} >
-                        <img src="./images/home/scindia.jpeg " className="card-img-top" alt="..." style={{
+                        <img src="./images/Kareena.jpg " className="card-img-top" alt="..." style={{
                             borderTopRightRadius: '12px',
                             borderTopLeftRadius: '12px',
                         }} />
-                        <div className="card-body card_h" style={{ borderRadius: '12px' }} >
-                            <h3 className="card-title  text-center" style={{ fontWeight: '500' }}>Scindia Kanya Vidyalaya</h3>
+                        <div className="card-body card_h " style={{ borderRadius: '12px', borderTopRightRadius:'0',borderTopLeftRadius:'0' }} >
+                            <h3 className="card-title  text-center" style={{ fontWeight: '500' }}> Kareena Kapoor Khan</h3>
                             <p className="card-text" style={{
                                 textAlign: 'justify',
                                 textJustify: 'interWord',
-                            }}>Scindia Kanya Vidyalaya is a premier girls' school, that helps children to grow both academically and personally. There is a 40% chance of your child, to study here.</p>
+                            }}> {des1.substring(0,190)}
+
+                            </p>
                             <div className="center">
                                 {/* <a href='/' style={{
                                     position: 'relative',
                                     top: '53px'
                                 }} className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a> */}
-                                <button className="btn-one btn_2" name="submit" type="submit" id="contact-submit" data-submit="...Sending">APPLY NOW</button>
+                                <button className="btn-one text-uppercase" name="submit" type="submit" id="contact-submit" data-submit="...Sending">Read More</button>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="col-md-6 col-lg-3 my-3">
-                    <div className="car" >
-                        <div className="card-body padd  " style={{ borderRadius: '12px' }} >
+                    <div className="card " style={{ border: 'none !important', borderTopRightRadius:'0',borderTopLeftRadius:'0' }} >
+                        <img src="./images/Meira.jpg " className="card-img-top" alt="..." style={{
+                            borderTopRightRadius: '12px',
+                            borderTopLeftRadius: '12px',
+                        }} />
+                        <div className="card-body card_h" style={{ borderRadius: '12px' }} >
+                            <h3 className="card-title  text-center" style={{ fontWeight: '500' }}>Meira Kumar</h3>
+                            <p className="card-text" style={{
+                                textAlign: 'justify',
+                                textJustify: 'interWord',
+                            }}> {des3.substring(0,190)}
 
-                            <form style={{ background: 'none' }} id="contact" className=" shadow-none padd" method="post" noValidate="novalidate">
-                                {/* <div className="kw-alert kw-alert-error">
-                                   </div> */}
-                                <div className="kw-alert kw-alert-success" id="kw-success" aria-live="assertive" style={{ display: 'none' }} />
-                                <h3 style={{ color: '#337ab7', fontWeight: 'bold', paddingTop: '10px', textTransform: 'uppercase', fontSize: '20px' }}>
-                                    Apply For Admissions</h3>
-                                <input style={{ marginTop: '5px' }} name="name" placeholder="Your name" type="text" tabIndex={1} required autoFocus className="kw-border-error" />
-                                <input style={{ marginTop: '5px' }} name="email" placeholder="Your Email Address" type="email" tabIndex={2} required />
-                                <input style={{ marginTop: '5px' }} name="PhoneNO" placeholder="Your Phone Number" type="tel" tabIndex={3} required />
-                                <button className="btn-one" style={{ marginBottom: '28px', marginTop: '20px' }} name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-                            </form>
+                            </p>
+                            <div className="center">
+                                {/* <a href='/' style={{
+                                    position: 'relative',
+                                    top: '53px'
+                                }} className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a> */}
+                                <button className="btn-one  text-uppercase" name="submit" type="submit" id="contact-submit" data-submit="...Sending">Read More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-3 my-3">
+                    <div className="card " style={{ border: 'none !important' }} >
+                        <img src="./images/Brinda.jpg " className="card-img-top" alt="..." style={{
+                            borderTopRightRadius: '12px',
+                            borderTopLeftRadius: '12px',
+                        }} />
+                        <div className="card-body card_h" style={{ borderRadius: '12px', borderTopRightRadius:'0',borderTopLeftRadius:'0' }} >
+                            <h3 className="card-title  text-center" style={{ fontWeight: '500' }}>Brinda Karat</h3>
+                            <p className="card-text" style={{
+                                textAlign: 'justify',
+                                textJustify: 'interWord',
+                            }}> {des3.substring(0,190)}
+
+                            </p>
+                            <div className="center">
+                                {/* <a href='/' style={{
+                                    position: 'relative',
+                                    top: '53px'
+                                }} className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a> */}
+                                <button className="btn-one text-uppercase" name="submit" type="submit" id="contact-submit" data-submit="...Sending">Read More</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-lg-3 my-3">
+                    <div className="card " style={{ border: 'none !important' }} >
+                        <img src="./images/Tavleen.jpg " className="card-img-top" alt="..." style={{
+                            borderTopRightRadius: '12px',
+                            borderTopLeftRadius: '12px',
+                        }} />
+                        <div className="card-body card_h" style={{ borderRadius: '12px', borderTopRightRadius:'0',borderTopLeftRadius:'0' }} >
+                            <h3 className="card-title  text-center" style={{ fontWeight: '500' }}>Tavleen Kaur</h3>
+                            <p className="card-text" style={{
+                                textAlign: 'justify',
+                                textJustify: 'interWord',
+                            }}> {des4.substring(0,190)} </p>
+                            <div className="center">
+                                {/* <a href='/' style={{
+                                    position: 'relative',
+                                    top: '53px'
+                                }} className="btn-one  scroll-to-target" data-target=".form-container">  Apply Now</a> */}
+                                <button className="btn-one text-uppercase" name="submit" type="submit" id="contact-submit" data-submit="...Sending">Read More</button>
+                            </div>
                         </div>
                     </div>
                 </div>
